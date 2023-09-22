6d1169a1ba4323371a2b1877184a24c706998e6c
comfortcat <103555147+Iwnat@users.noreply.github.com>
2023-09-22 14:15:29 +0900

    sub2.js

--- sub2.js
+++ sub2.js
@@ -1,55 +1,6 @@
--const key = "?key=63963d0c691029be0863c9839473b6d4";
-const itemPerPage = "&itemPerPage=20";
-const url =
-  "http://kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json" +
-  key +
-  itemPerPage;
-
-const title = document.getElementById("Info");
-
-fetch(url)
-  .then((response) => response.json())
-  .then((msg) => {
-    console.log(msg);
-    for (let i = 0; i < 20; i++) {
-      let div = document.createElement("div");
-      let low_div = document.createElement("div");
-      let title_div = document.createElement("div");
-
-      let mp = document.createElement("p");
-      mp.id = "movieNm";
-      let dp = document.createElement("p");
-      dp.id = "directors";
-      let op = document.createElement("p");
-      op.id = "openDt";
-
-      title_div.id = "title_div";
-      low_div.id = "low_div";
-      div.id = "movies";
-
-      short_url = msg.movieListResult.movieList[i];
-      console.log(short_url);
-
-      div.appendChild(title_div);
-      div.appendChild(low_div);
-      title.appendChild(div);
-
-      let movieNm = document.createTextNode(short_url.movieNm);
-      let directors = document.createTextNode(short_url.directors);
-      let openDt = document.createTextNode(short_url.openDt);
-
-      mp.appendChild(movieNm);
-      dp.appendChild(directors);
-      op.appendChild(openDt);
-
-      title_div.appendChild(mp);
-      low_div.appendChild(dp);
-      low_div.appendChild(op);
-    }
-  });
 function fetchMovies() {
   const KEY = "63963d0c691029be0863c9839473b6d4";
-  const itemPerPage = "&itemPerPage=5";
+  const itemPerPage = "&itemPerPage=3";
 
   const loadingElement = document.createElement("h2");
   loadingElement.textContent = "영화 정보";
@@ -60,7 +11,7 @@ function fetchMovies() {
   )
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Network response was not ok");
+        throw new Error("Error");
       }
       return response.json();
     })
@@ -78,12 +29,12 @@ function fetchMovies() {
         const movieNmElement = document.createElement("p");
         movieNmElement.textContent = `영화 제목 : ${Info.movieNm}`;
 
-        const directorsElement = document.createElement("p");
-        directorsElement.textContent = `영화 감독 : (${Info.directors})`;
+        const genreAltElement = document.createElement("p");
+        genreAltElement.textContent = `영화 장르 : (${Info.genreAlt})`;
 
         movieElement.appendChild(movieTitleElement);
         movieElement.appendChild(movieNmElement);
-        movieElement.appendChild(directorsElement);
+        movieElement.appendChild(genreAltElement);
 
         moviesContainer.appendChild(movieElement);
       });
@@ -96,7 +47,7 @@ function fetchMovies() {
       document.getElementById("Info").appendChild(moviesContainer);
     })
     .catch((error) => {
-      console.error("Error fetching data:", error);
+      console.error("Error:", error);
     });
 }
 
