7bb411c9f27173c056d04d6e6d9d9e4e1f368c34
demiso5 <133202457+demiso5@users.noreply.github.com>
2023-09-22 15:09:42 +0900

    Add files via upload

---
+++ login2.js
@@ -0,0 +1,27 @@
+const id = document.getElementById('_id');
+const pw = document.getElementById('_pw');
+const loginBtn = document.getElementById('login-btn'); 
+const alert1 = document.querySelector('.alert1');
+const alert2 = document.querySelector('.alert2');
+
+loginBtn.addEventListener('click', (e) => {
+  if(id.value == ''){
+    e.preventDefault();
+    alert1.classList.add('alert-id');
+  } else if(pw.value == ''){
+    e.preventDefault();
+    alert1.classList.remove('alert-id');
+    alert2.classList.add('alert-pw');
+  } 
+})
+
+
+// loginBtn.addEventListener('click', (e) => {
+//   if(id.value == ''){
+//     e.preventDefault();
+//     alert('아이디를 입력하세요')
+//   } else if(pw.value == ''){
+//     e.preventDefault();
+//     alert('비밀번호를 입력하세요')
+//   } 
+// })
