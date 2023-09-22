6bd2c67c290fa37dd76c8ca68d0a7d6e384c8f17
demiso5 <133202457+demiso5@users.noreply.github.com>
2023-09-22 15:23:02 +0900

    Add files via upload
    login 화면 최종수정

--- login2.js
+++ login2.js
@@ -4,24 +4,24 @@ const loginBtn = document.getElementById('login-btn');
 const alert1 = document.querySelector('.alert1');
 const alert2 = document.querySelector('.alert2');
 
-loginBtn.addEventListener('click', (e) => {
-  if(id.value == ''){
-    e.preventDefault();
-    alert1.classList.add('alert-id');
-  } else if(pw.value == ''){
-    e.preventDefault();
-    alert1.classList.remove('alert-id');
-    alert2.classList.add('alert-pw');
-  } 
-})
-
-
 // loginBtn.addEventListener('click', (e) => {
 //   if(id.value == ''){
 //     e.preventDefault();
-//     alert('아이디를 입력하세요')
+//     alert1.classList.add('alert-id');
 //   } else if(pw.value == ''){
 //     e.preventDefault();
-//     alert('비밀번호를 입력하세요')
+//     alert1.classList.remove('alert-id');
+//     alert2.classList.add('alert-pw');
 //   } 
 // })
+
+
+loginBtn.addEventListener('click', (e) => {
+  if(id.value == ''){
+    e.preventDefault();
+    alert('아이디를 입력하세요')
+  } else if(pw.value == ''){
+    e.preventDefault();
+    alert('비밀번호를 입력하세요')
+  } 
+})
